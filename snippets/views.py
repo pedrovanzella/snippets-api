47ec85b995595a03c47a2a65489425498675dd95
Pedro Vanzella <pedro@pedrovanzella.com>
2017-04-12 16:03:51 -0300

    Remove unnecessary view

--- snippets/views.py
+++ snippets/views.py
@@ -3,9 +3,8 @@ from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import permissions, renderers, viewsets
 from django.contrib.auth.models import User
 from snippets.permissions import IsOwnerOrReadOnly
-from rest_framework.decorators import api_view, detail_route
+from rest_framework.decorators import detail_route
 from rest_framework.response import Response
-from rest_framework.reverse import reverse
 
 
 class SnippetViewSet(viewsets.ModelViewSet):
@@ -35,11 +34,3 @@ class UserViewSet(viewsets.ReadOnlyModelViewSet):
     """
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-
-@api_view(['GET'])
-def api_root(request, format=None):
-    return Response({
-        'users': reverse('user-list', request=request, format=format),
-        'snippets': reverse('snippet-list', request=request, format=format)
-    })
