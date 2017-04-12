13f5d8a17b58445cdf79a154e9d1be00ef4c2e7e
Pedro Vanzella <pedro@pedrovanzella.com>
2017-04-12 16:25:47 -0300

    Add schema API view

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,16 +1,20 @@
 from django.conf.urls import url, include
 from snippets import views
 from rest_framework.routers import DefaultRouter
+from rest_framework.schemas import get_schema_view
 
 # Create a router and register our viewsets with it.
 router = DefaultRouter()
 router.register(r'snippets', views.SnippetViewSet)
 router.register(r'users', views.UserViewSet)
 
+schema_view = get_schema_view(title='Snippets API')
+
 # The API URLs are now determined automatically by the router.
 # Additionally, we include the URLs for the browsable API.
 urlpatterns = [
     url(r'^', include(router.urls)),
+    url(r'^schema/$', schema_view),
     url(r'^api-auth/', include('rest_framework.urls',
                                namespace='rest_framework')),
 ]
